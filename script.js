0928300d73fc7bfd642808c647e0aacee447ac2e
juliogazoli <julio_gazoli@hotmail.com>
2023-09-01 12:58:13 -0300

    Desafio: adicionando evento de click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
@@ -8,4 +9,8 @@ focoBt.addEventListener('click', () => {
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
 })
